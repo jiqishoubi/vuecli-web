00581dd75663992974febdc44cb1f46c3f0374e3
佟舟 <437753880@qq.com>
2021-11-02 17:53:48 +0800

    面包屑

--- src/components/layout/Header/index.vue
+++ src/components/layout/Header/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="header_box">
-    <div class="header_content"></div>
+    <div class="header_content">
+      <!-- 搞一个面包屑 -->
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item v-for="(item, index) in breadcrumbItems" :key="index">
+          {{ item.menuTitle }}
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
     <div class="header_user_content">
       <HeaderUserContent />
     </div>
@@ -11,15 +18,20 @@
 import { computed } from "vue";
 import { useStore } from "vuex";
 import HeaderUserContent from "../HeaderUserContent/index.vue";
+import useRouterBreadcrumb from "./useRouterBreadcrumb";
 
 export default {
   setup() {
     const store = useStore();
     const userInfo = computed(() => store.state.user.userInfo);
     const menuTree = computed(() => store.state.user.menuTree);
+
+    const breadcrumbItems = useRouterBreadcrumb();
+
     return {
       userInfo,
       menuTree,
+      breadcrumbItems,
     };
   },
   components: {
